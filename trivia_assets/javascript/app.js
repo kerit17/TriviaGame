e689db468f2fcf31e103654540c03239bbf17bce
Keri Schreckengost <keris@live.com>
2017-06-04 20:17:11 -0600

    questions and radio buttons

--- trivia_assets/javascript/app.js
+++ trivia_assets/javascript/app.js
@@ -1,61 +1,60 @@
 $(document).ready(function() {
 
-function hide(){
-	$("#hide").load(function(){
-	$("p").hide();
-	document.getElementById("quiz");
+	// scripts here:
 
-	
+	function submitQuiz() {
+		console.log('submitted');
+
+	// get each answer score
+		function answerScore (qName) {
+			var radiosNo = document.getElementsByName(qName);
+
+			for (var i = 0, length = radiosNo.length; i < length; i++) {
+   				if (radiosNo[i].checked) {
+			// do something with radiosNo
+					var answerValue = Number(radiosNo[i].value);
+				}
+			}
+			// change NaNs to zero
+			if (isNaN(answerValue)) {
+				answerValue = 0;
+			}
+			return answerValue;
+		}
+
+	// calc score with answerScore function
+		var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
+		console.log("CalcScore: " + calcScore); // it works!
+
+	// function to return correct answer string
+		function correctAnswer (correctStringNo, qNumber) {
+			console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
+			return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
+				(document.getElementById(correctStringNo).innerHTML) + "</strong>");
+			}
+
+	// calculate "possible score" integer
+		var questionCountArray = document.getElementsByClassName('question');
+
+		var questionCounter = 0;
+		for (var i = 0, length = questionCountArray.length; i < length; i++) {
+			questionCounter++;
+		}
+
+	// show score as "score/possible score"
+		var showScore = "Your Score: " + calcScore +"/" + questionCounter;
+	// if 4/4, "perfect score!"
+		if (calcScore === questionCounter) {
+			showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
+		};
+		document.getElementById('userScore').innerHTML = showScore;
+	}
+
+$(document).ready(function() {
+
+	$('#submitButton').click(function() {
+		$(this).addClass('hide');
+	})
 })
 
-$("#show").click(function(){
-        $("p").show();
-    });
-
-// var one = {
-// 	question: "Q1. The first recorded watermelon harvest occurred nearly 5,000 years ago in Egypt.",
-// 	option1: "True",
-// 	option2: "False",
-// 	answer: "True",
-// };
-
-// var two = {
-// 	question: "	",
-// 	option1: "True",
-// 	option2: "False",
-// 	answer: "False",
-// };
-
-// var three = {
-// 	question: "Q3. In Israel and Egypt, the sweet taste of watermelon is often paired with the salty taste of feta cheese.",
-// 	option1: "True",
-// 	option2: "False",
-// 	answer: "True",
-// };
-
-// var questionsArray = [one, two, three];
-
-// //on-click append timer and questions to after instructions
-// function trivia (x) {
-// 	$("#start").click(function(gameSetUp){
-// 		$("p").append("<p> class="one.question">"
-	// };
-		// $("p").append(	);
-		// $("p").append(	);
-
-
-// }) 
-
-
-
-//questions, options, and answers object - one object for each
-
-
-//questions array
-
-
-//
-
-
-
-}) //closing brackets
+})
