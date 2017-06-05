7faee044e018321c6abdf8a241544b38b041258e
Keri Schreckengost <keris@live.com>
2017-06-04 19:03:02 -0600

    draft

---
+++ trivia_assets/javascript/app.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+function hide(){
+	$("#hide").load(function(){
+	$("p").hide();
+	document.getElementById("quiz");
+
+	
+})
+
+$("#show").click(function(){
+        $("p").show();
+    });
+
+// var one = {
+// 	question: "Q1. The first recorded watermelon harvest occurred nearly 5,000 years ago in Egypt.",
+// 	option1: "True",
+// 	option2: "False",
+// 	answer: "True",
+// };
+
+// var two = {
+// 	question: "	",
+// 	option1: "True",
+// 	option2: "False",
+// 	answer: "False",
+// };
+
+// var three = {
+// 	question: "Q3. In Israel and Egypt, the sweet taste of watermelon is often paired with the salty taste of feta cheese.",
+// 	option1: "True",
+// 	option2: "False",
+// 	answer: "True",
+// };
+
+// var questionsArray = [one, two, three];
+
+// //on-click append timer and questions to after instructions
+// function trivia (x) {
+// 	$("#start").click(function(gameSetUp){
+// 		$("p").append("<p> class="one.question">"
+	// };
+		// $("p").append(	);
+		// $("p").append(	);
+
+
+// }) 
+
+
+
+//questions, options, and answers object - one object for each
+
+
+//questions array
+
+
+//
+
+
+
+}) //closing brackets
